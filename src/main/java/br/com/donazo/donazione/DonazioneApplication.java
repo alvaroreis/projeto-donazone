cd89f5a9cfdb257f89baa8b8fa14697a5d9cfd33
Alvaro Barros <alvarobarros@iec.gov.br>
2019-04-08 16:32:52 -0300

    Atualização

--- src/main/java/br/com/donazo/donazione/DonazioneApplication.java
+++ src/main/java/br/com/donazo/donazione/DonazioneApplication.java
@@ -2,13 +2,13 @@ package br.com.donazo.donazione;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class DonazioneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DonazioneApplication.class, args);
-	}
+    public static void main(final String[] args) {
+
+        SpringApplication.run(DonazioneApplication.class, args);
+    }
 
 }
