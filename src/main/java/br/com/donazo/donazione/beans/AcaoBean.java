624c36f2c5db530a1919f3358e52fea1c4aa96cc
Alvaro Barros <alvaroluiz.rb@gmail.com>
2019-04-10 23:30:18 -0300

    Inserção de classes utilitarias e atualização das paginas relacionadas a ação.

--- src/main/java/br/com/donazo/donazione/beans/AcaoBean.java
+++ src/main/java/br/com/donazo/donazione/beans/AcaoBean.java
@@ -4,74 +4,123 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.CloseEvent;
+
 import br.com.donazo.donazione.entities.Acao;
 import br.com.donazo.donazione.repositorios.AcaoRepository;
+import br.com.donazo.donazione.utils.MessagesUtil;
 
 @Named
-@RequestScoped
+@ViewScoped
 public class AcaoBean implements Serializable {
 
-	private static final long serialVersionUID = -858774744832095173L;
+    private static final long serialVersionUID = -858774744832095173L;
 
-	private Acao acao;
+    private Acao acao;
 
-	private Iterable<Acao> acoes;
+    private Acao acaoSelecionado;
 
-	@Inject
-	private AcaoRepository acaoRepository;
+    private Iterable<Acao> acoes;
 
-	@PostConstruct
-	public void init() {
-		this.acao = new Acao();
-		this.acao.setCadastro(new Date());
-		this.acoes = this.acaoRepository.findAll();
-	}
+    @Inject
+    private AcaoRepository acaoRepository;
 
-	public void prepaprar() {
-		this.acao = new Acao();
-		this.acao.setCadastro(new Date());
-	}
+    @PostConstruct
+    public void init() {
 
-	public String gravarInserir() {
+        this.acoes = this.acaoRepository.findAll();
+    }
 
-		this.acaoRepository.save(this.acao);
-		return "/pages/acao/lista.xhtml";
-	}
+    public void prepaprar() {
 
-	/**
-	 * @return the acao
-	 */
-	public Acao getAcao() {
+        if (this.acao != null) {
+            this.acao = new Acao();
+            this.acao.setCargaHoraria(1);
+            this.acao.setCadastro(new Date());
+        }
 
-		return this.acao;
-	}
+    }
 
-	/**
-	 * @param acao the acao to set
-	 */
-	public void setAcao(final Acao acao) {
+    public void gravarInserir() {
 
-		this.acao = acao;
-	}
+        try {
+            this.acaoRepository.save(this.acao);
+            this.prepaprar();
+            MessagesUtil.criarMensagemDeInformacao("Ação " + this.acao.getNome() + "  salva.");
+        } catch (final Exception e) {
+            MessagesUtil.criarMensagemDeErro(e.getMessage());
+        }
+    }
 
-	/**
-	 * @return the acoes
-	 */
-	public Iterable<Acao> getAcoes() {
+    public void gravarExcluir() {
 
-		return this.acoes;
-	}
+        try {
+            MessagesUtil.criarMensagemDeInformacao("Ação " + this.acao.getNome() + "  excluída.");
+            this.acaoRepository.delete(this.acao);
+        } catch (final Exception e) {
+            MessagesUtil.criarMensagemDeErro(e.getMessage());
+        }
 
-	/**
-	 * @param acoes the acoes to set
-	 */
-	public void setAcoes(final Iterable<Acao> acoes) {
+    }
 
-		this.acoes = acoes;
-	}
+    public void closeEvent(final CloseEvent closeEvent) {
+
+        this.init();
+    }
+
+    /**
+     * @return the acao
+     */
+    public Acao getAcao() {
+
+        return this.acao;
+    }
+
+    /**
+     * @param acao
+     *            the acao to set
+     */
+    public void setAcao(final Acao acao) {
+
+        this.acao = acao;
+    }
+
+    /**
+     * @return the acaoSelecionado
+     */
+    public Acao getAcaoSelecionado() {
+
+        return this.acaoSelecionado;
+    }
+
+    /**
+     * @param acaoSelecionado
+     *            the acaoSelecionado to set
+     */
+    public void setAcaoSelecionado(final Acao acaoSelecionado) {
+
+        this.acaoSelecionado = acaoSelecionado;
+    }
+
+    /**
+     * @return the acoes
+     */
+    public Iterable<Acao> getAcoes() {
+
+        return this.acoes;
+    }
+
+    /**
+     * @param acoes
+     *            the acoes to set
+     */
+    public void setAcoes(final Iterable<Acao> acoes) {
+
+        this.acoes = acoes;
+    }
 
 }
